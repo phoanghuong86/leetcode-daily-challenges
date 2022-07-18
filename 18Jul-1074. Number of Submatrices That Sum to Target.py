8fc1b43e582f7cf778b3a4c7e21b3171099638f1
Pho Hoang Huong <phoanghuong86@yahoo.com>
2022-07-18 09:44:07 +0700

    Create 18Jul-1074. Number of Submatrices That Sum to Target.py

---
+++ 18Jul-1074. Number of Submatrices That Sum to Target.py
@@ -0,0 +1,25 @@
+class Solution:
+    def numSubmatrixSumTarget(self, matrix: List[List[int]], target: int) -> int:
+        m,n = len(matrix), len(matrix[0])
+        for i in range(m):
+            for j in range(1,n):
+                matrix[i][j] += matrix[i][j-1]
+        
+        # print(matrix)
+        ans = 0
+        #submatrix column division for each pair of columns
+        for sc1 in range(n):
+            for sc2 in range(sc1,n):
+                d = collections.defaultdict(int)
+                ## sum 0
+                d[0] = 1
+                #submatrix sum
+                s = 0
+                for i in range(m):
+                    s += matrix[i][sc2]
+                    #remove previous column sums
+                    if sc1:
+                        s -= matrix[i][sc1-1]
+                    ans += d[s-target]
+                    d[s] += 1
+        return ans
