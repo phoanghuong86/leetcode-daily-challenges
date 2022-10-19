03cc40d850c000569ceaf4da8edd191535522c43
Pho Hoang Huong <phoanghuong86@yahoo.com>
2022-10-19 09:26:04 +0700

    Create 19Oct-692. Top K Frequent Words.py

---
+++ 19Oct-692. Top K Frequent Words.py
@@ -0,0 +1,11 @@
+class Solution:
+    def topKFrequent(self, words: list[str], k: int) -> list[str]:
+	
+                                                    # Ex: words = ["i","love","leetcode","i","love","coding"], k = 2
+													
+        c = Counter(words)                          #         c = {'i': 2, 'love': 2, 'leetcode': 1, 'coding': 1}
+        
+        ans = sorted(c, key = lambda x: (-c[x],x))  #       ans = ['i', 'love', 'coding', 'leetcode']
+                                                    #             sort keys are [(-2,'i'), (-2,'love'), (-1,'coding'), (-1,'leetcode')]
+
+        return ans[:k]                              #   ans[:2] = ['i', 'love']
